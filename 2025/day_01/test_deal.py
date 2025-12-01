8b656a69e90c18248a379e4e079d037096c4b04a
Ingo Reschke <kontakt@ingo-reschke.de>
2025-12-01 23:29:26 +0100

    extract test parameters

--- 2025/day_01/test_deal.py
+++ 2025/day_01/test_deal.py
@@ -4,6 +4,19 @@ from dial import rotate_dial
 
 
 class TestDialRotation(unittest.TestCase):
+    parameters = [
+        ('L', 68),
+        ('L', 30),
+        ('R', 48),
+        ('L', 5),
+        ('R', 60),
+        ('L', 55),
+        ('L', 1),
+        ('L', 99),
+        ('R', 14),
+        ('L', 82),
+    ]
+
     def test_left_rotation(self):
         self.assertEqual(rotate_dial(50, 'L', 68), 82)
 
@@ -12,22 +25,9 @@ class TestDialRotation(unittest.TestCase):
 
     def test_sequence(self):
         result = 50
-        parameters = [
-            ('L', 68),
-            ('L', 30),
-            ('R', 48),
-            ('L', 5),
-            ('R', 60),
-            ('L', 55),
-            ('L', 1),
-            ('L', 99),
-            ('R', 14),
-            ('L', 82),
-        ]
 
-        for direction, value in parameters:
+        for direction, value in self.parameters:
             result = rotate_dial(result, direction, value)
-
             print(f"The dial is rotated {direction} {value} to point at {result}")
 
         self.assertEqual(result, 32)
